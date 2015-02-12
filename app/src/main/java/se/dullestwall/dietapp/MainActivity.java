193a5a3c8838c42f21cc26138f2287cad9e21360
Patsi <patrik.stigeborn@gmail.com>
2015-02-12 12:58:34 +0100

    Fixed Lagg issue, added daily recipe fragment and related classes

--- app/src/main/java/se/dullestwall/dietapp/MainActivity.java
+++ app/src/main/java/se/dullestwall/dietapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -17,6 +18,7 @@ import android.support.v4.widget.DrawerLayout;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -36,7 +38,9 @@ public class MainActivity extends ActionBarActivity implements RecipesFragment.O
     /**
      * List of recipes
      */
-    private List<Recipe> recipes;
+    public static List<Recipe> recipes;
+    //Saving Recipes in a HashMap, String=Day
+    public static HashMap<String,Recipe> weekRecipes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +49,24 @@ public class MainActivity extends ActionBarActivity implements RecipesFragment.O
 
         // Loads recipes from Json file
         loadRecipes();
+        //Randomize recipes for each day
+        DailyRandomRecipe daily = new DailyRandomRecipe();
+        weekRecipes = new HashMap<String, Recipe>();
+        int random = daily.getRandomRecipeDay(recipes.size());
+        weekRecipes.put("Monday",recipes.get(random));
+        random = daily.getRandomRecipeDay(recipes.size());
+        weekRecipes.put("Tuesday",recipes.get(random));
+        random = daily.getRandomRecipeDay(recipes.size());
+        weekRecipes.put("Wednesday",recipes.get(random));
+        random = daily.getRandomRecipeDay(recipes.size());
+        weekRecipes.put("Thursday",recipes.get(random));
+        random = daily.getRandomRecipeDay(recipes.size());
+        weekRecipes.put("Friday",recipes.get(random));
+        random = daily.getRandomRecipeDay(recipes.size());
+        weekRecipes.put("Saturday",recipes.get(random));
+        random = daily.getRandomRecipeDay(recipes.size());
+        weekRecipes.put("Sunday",recipes.get(random));
+
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -61,6 +83,7 @@ public class MainActivity extends ActionBarActivity implements RecipesFragment.O
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
         FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction;
         switch(position)
         {
             case 0:
@@ -71,17 +94,19 @@ public class MainActivity extends ActionBarActivity implements RecipesFragment.O
                 break;
             case 1:
 
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, RecipesFragment.newInstance("test2", Integer.toString(position)))
-                        .commit();
+                transaction = getSupportFragmentManager().beginTransaction();
+                RecipesFragment fragmentAllRec = new RecipesFragment();
+                transaction.replace(R.id.container, fragmentAllRec);
+                transaction.commit();
 
                 break;
 
             case 2:
                 //placeholder
-                fragmentManager.beginTransaction()
-                        .replace(R.id.container, PlaceholderFragment.newInstance(position))
-                        .commit();
+                transaction = getSupportFragmentManager().beginTransaction();
+                DailyViewFragment fragmentDaily = new DailyViewFragment();
+                transaction.replace(R.id.container, fragmentDaily);
+                transaction.commit();
                 break;
         }
 
